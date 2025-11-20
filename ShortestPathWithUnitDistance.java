ef915744dfa62d7eebbc4ebe486260a255b8a42f
Saketh Reddy <sakethreddynsr@gmail.com>
2025-11-19 18:35:50 -0800

    ShortestPathWithUnitDistance

---
+++ ShortestPathWithUnitDistance.java
@@ -0,0 +1,28 @@
+public class ShortestPathWithUnitDistance {
+    public int shortestPath(int[][] edges) {
+        int n = edges.length;
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int next : graph[cur]) {
+                if (dist[next] > dist[cur] + 1) {
+                    dist[next] = dist[cur] + 1;
+                    queue.offer(next);
+                }
+            }
+        }
+        return dist[n - 1];
+    }
+}
